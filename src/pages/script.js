3d1f1234b17f4db39ab82b1322684426705992ee
David Escobar <gadianton.sett@gmail.com>
2023-11-02 19:14:51 -0600

    Made major organizational changes to all HTML files by separating inline CSS and JS into individual files.

---
+++ src/pages/script.js
@@ -0,0 +1,48 @@
+// This is the entry point for the application
+// This file is responsible for importing all the other files
+
+// JS Section for the index.html page
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Mock database for product data
+    const mockDatabase = [
+        { id: 1, name: "Product 1", description: "Some description for Product 1" },
+        { id: 2, name: "Product 2", description: "Some description for Product 2" },
+    ];
+
+    // Display a welcome message if user is logged in
+    const userEmail = localStorage.getItem("userEmail");
+    if (userEmail) {
+        document.getElementById("username_placeholder").textContent = userEmail;
+    } else {
+        document.getElementById("username_placeholder").textContent = "Guest";
+    }
+
+    // Populate products to the page
+    const displayProducts = () => {
+        const productsDiv = document.createElement("div");
+        productsDiv.classList.add("products");
+
+        mockDatabase.forEach(product => {
+            const productDiv = document.createElement("div");
+            productDiv.classList.add("product");
+
+            const productName = document.createElement("h2");
+            productName.textContent = product.name;
+
+            const productDesc = document.createElement("p");
+            productDesc.textContent = product.description;
+
+            productDiv.appendChild(productName);
+            productDiv.appendChild(productDesc);
+
+            productsDiv.appendChild(productDiv);
+        });
+
+        // Append the productsDiv to a specific section in your HTML
+        // Replace "products_section" with the actual id of the section where you want to display the products
+        document.getElementById("products_section").appendChild(productsDiv);
+    };
+
+    displayProducts();
+});
